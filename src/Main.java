94662dab706b6b2077941616e0a935ba9749f99f
Abhinav <abhinavsv3@gmail.com>
2015-03-14 12:58:58 +0530

    Login JFrame under Construction

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author abhinav
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VeloxOpening v = new VeloxOpening();
+        v.setVisible(true);
+    }
+}
